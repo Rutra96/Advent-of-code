f5206c05745f0e8b062ef4ed90f474a40aaec246
Rutra96 <59706898+Rutra96@users.noreply.github.com>
2020-01-10 20:38:42 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-    
-    Scanner myObj = new Scanner(System.in);
-
-    System.out.println("Enter module mass:");
-
-    int mass = myObj.nextInt();
+  public static void main(String[] args) {
+    try{
+      File x = new File(//);
+      Scanner sc = new Scanner(x);
+      while(sc.hasNextInt()){
+        int mass = sc.nextInt();
+        System.out.println(mass);
+      }
+      sc.close();
+    }catch(FileNotFoundException e){
+      System.out.println("Error");
+    }
+ 
     int sum = 0;
     
     sum = mass/3-2;
   
     System.out.println(sum);
+
+
 
   }
 }
